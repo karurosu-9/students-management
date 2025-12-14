477ce8f7b8bc355de05f1935a5de53bf3a7b0e6a
carlos <cl0xtc43x.apxma@gmail.com>
2025-12-15 02:25:38 +0900

    生徒登録画面の作成と登録処理の実装

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Resources\ClassesResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -16,5 +19,27 @@ class StudentController extends Controller
         return Inertia::render('Students/Index', [
             'students' => $students
         ]);
+    }
+
+    public function create()
+    {
+        // ResourceクラスでJSON化して全てのクラスを取得
+        $classes = ClassesResource::collection(Classes::all());
+
+        return Inertia::render('Students/Create', [
+            'classes' => $classes,
+        ]);
+    }
+
+    public function store(StoreStudentRequest $request)
+    {
+        Student::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'class_id' => $request->class_id,
+            'section_id' => $request->section_id
+        ]);
+
+        return redirect(route('students.index'));
     }
 }
